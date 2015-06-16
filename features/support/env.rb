8d0a33c2ecc321168eddbb785fc9dfb316ea0656
drsoffice <drs0ffic3@users.noreply.github.com>
2015-06-16 19:26:26 -0400

    Remove ruby version + fix rubocop

--- features/support/env.rb
+++ features/support/env.rb
@@ -18,7 +18,7 @@ ActionController::Base.allow_rescue = false
 begin
   DatabaseCleaner.strategy = :truncation
 rescue NameError
-  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+  raise 'You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it.'
 end
 
 Capybara.register_driver :poltergeist do |app|
